2e0f29917ea56338cec5393a653ad389a7c31043
Junaid Babu <junaidvayoli@gmail.com>
2014-01-13 03:03:53 +0530

    Bootstrapped

--- home.php
+++ home.php
@@ -1,30 +1,56 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+session_start();
+?>
 <html>
   <head>
     <!--Load the AJAX API-->
-    <title>My Awesome Stat site</title>
+    <title>
+      My Awesome Stat site
+    </title>
     <style type="text/css">
-    #loading{
-    	position: absolute;
-		top: 50%;
-		left: 50%;
-		z-index: 9999;
-		display: none;
-}
+      #loading{
+      position: absolute;
+    top: 50%;
+    left: 50%;
+    z-index: 9999;
+    display: none;
+      }
+      body {
+    padding-top: 60px;
+  }
+    </style>
+    <link href="http://eternicode.github.io/bootstrap-datepicker/bootstrap-datepicker/css/datepicker.css" rel="stylesheet">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+    </script>
 
- </style>
- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> 
- <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
- <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
     
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js">
+    </script>
+    
+    <script src="http://eternicode.github.io/bootstrap-datepicker/google-code-prettify/prettify.min.js"></script>
+    <script src="http://eternicode.github.io/bootstrap-datepicker/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+    
+    <script type="text/javascript" src="https://www.google.com/jsapi">
+    </script>
     <script type="text/javascript">
-
+     $(document).ready(function () {
+                
+                $('#sandbox-container .input-daterange').datepicker({
+    format: "yyyy-mm-dd"
+});
+            
+            });
       // Load the Visualization API and the piechart package.
-      google.load('visualization', '1.0', {'packages':['corechart']});
-
+      google.load('visualization', '1.0', {'packages':['corechart']}
+                 );
+      
       // Set a callback to run when the Google Visualization API is loaded.
       //google.setOnLoadCallback(drawChart);
-
+      
       // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
       // draws it.
@@ -38,48 +64,51 @@
           return hours+' hours, '+minutes+' minutes and '+seconds+' seconds';
         return days+' days, '+hours+' hours, '+minutes+' minutes and '+seconds+' seconds';
       }
-
+      
       function count(total, arg){
-      	var count=0;
-      		for (var i = total.length - 1; i >= 0; i--) {
-      			//console.log(total[i].type.summary);
-      			if(arg=="outa"){
-      				if("outgoing "==total[i].type){
-	      				if(total[i].time>0)
-	      					count++;
-      				}
-
-      			}if(arg=="out"){
-      				if("outgoing "==total[i].type){
-	      				if(total[i].time==0)
-	      					count++;
-      				}
-
-      			}else if(arg==total[i].type)
-      				count++;
-      		}
-      	return count;      
+        var count=0;
+        for (var i = total.length - 1; i >= 0; i--) {
+          //console.log(total[i].type.summary);
+          if(arg=="outa"){
+            if("outgoing "==total[i].type){
+              if(total[i].time>0)
+                count++;
+            }
+            
+          }
+          if(arg=="out"){
+            if("outgoing "==total[i].type){
+              if(total[i].time==0)
+                count++;
+            }
+            
+          }
+          else if(arg==total[i].type)
+            count++;
+        }
+        return count;
+        
       }
       function gettotal(total, arg){
-      	var count=0;
-      	for (var i = total.length - 1; i >= 0; i--) {
-      		if(arg==total[i].type){
-      			count+=total[i].time;
-       		}
-      	}
-      	console.log(count);
-      	return count;
+        var count=0;
+        for (var i = total.length - 1; i >= 0; i--) {
+          if(arg==total[i].type){
+            count+=total[i].time;
+          }
+        }
+        console.log(count);
+        return count;
       }
-
+      
       function drawChart() {
-
-      	//gettotal(totaldata, "outgoing ");
-/*
+        
+        //gettotal(totaldata, "outgoing ");
+        /*
 ['incoming', count(totaldata, "incoming ")],
-          ['outgoing-actuall', count(totaldata, "outa")],
-          ['outgoing-missed', count(totaldata, "out")],
-          ['missed', count(totaldata, "missed ")]
-          */
+['outgoing-actuall', count(totaldata, "outa")],
+['outgoing-missed', count(totaldata, "out")],
+['missed', count(totaldata, "missed ")]
+*/
         // Create the data table.
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Topping');
@@ -90,13 +119,14 @@
           ['outgoing-missed', count(totaldata, "out")],
           ['missed', count(totaldata, "missed ")]
         ]);
-
+        
         // Set chart options
         var options = {'title':'My total Calls: '+totaldata.length,
                        'width':1200,
-                       'height':300};
-
-
+                       'height':300}
+            ;
+        
+        
         
         t1=gettotal(totaldata, "incoming ");
         t2=gettotal(totaldata, "outgoing ");
@@ -108,15 +138,16 @@
           ['outgoing '+fintime(t2), t2]
           
         ]);
-
+        
         var options2 = {
           title: 'My Total Talk Times: '+fintime(t1+t2),
           is3D: true,
           'width':1200,
           'height':300
-        };
-
-
+        }
+            ;
+        
+        
         // Instantiate and draw our chart, passing in some options.
         var chart = new google.visualization.PieChart(document.getElementById('chart_div1'));
         chart.draw(data, options);
@@ -126,36 +157,85 @@
       var totaldata;
       $("#loading").hide();
       function Plot(){
-      	var start = $('#start').val();
-      	var end = $('#end').val();
-      	url = "get.php?start="+start+"&end="+end;
-      	//alert(url);
-      	$("#loading").show();
-		$.get(url, function( data ) {
-		  totaldata=JSON.parse(data);
-		  //console.log(totaldata);
-		 // obj = JSON.parse(totaldata);
-		 // alert(obj[0].id);
-		  google.setOnLoadCallback(drawChart);
-		  $("#loading").hide();
-		  drawChart();
-		});
+        var start = $('#start').val();
+        var end = $('#end').val();
+        url = "get.php?start="+start+"&end="+end;
+        //alert(url);
+        $("#loading").show();
+    $.get(url, function( data ) {
+      totaldata=JSON.parse(data);
+      //console.log(totaldata);
+          // obj = JSON.parse(totaldata);
+          // alert(obj[0].id);
+      google.setOnLoadCallback(drawChart);
+      $("#loading").hide();
+      drawChart();
+    }
+             );
       }
-
+      
     </script>
   </head>
-<div id="loading">
+  <div id="loading">
     <img src="http://i.imgur.com/MPw8nvR.gif"/>
-</div>
+  </div>
   <body>
-  Start Date: <input id="start" type="date" name="start">
-  End Date: <input id="end" type="date" name="end">
-  <button class="btn btn-default" onclick="Plot()">Plot</button>
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Call Log Analytics</a>
+  </div>
 
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">How to...</a></li>
+      <li><a href="#">About Us</a></li>
+    </ul>
+    <div class="navbar-form navbar-left">
+      <div id="sandbox-container"><div class="input-daterange" id="datepicker">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Start Date" id="start"name="start">
+      </div>
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="End Date" id="end" name="end" autocomplete="off">
+      </div>
+      <button class="btn btn-default" onclick="Plot()">Plot</button>
+      </div></div>
+      
+    </div>
+    <ul class="nav navbar-nav navbar-right" style="
+    padding-right: 20;
+">
+      
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['email']; ?><b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="./?change">Change Calendar</a></li>
+          <li class="divider"></li>
+          <li><a href="./?logout">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
+    
+     
 
+    
 
+   <div class="container">
     <!--Div that will hold the pie chart-->
-    <div id="chart_div1"></div>
-    <div id="chart_div2"></div>
+    <div id="chart_div1">
+    </div>
+    <div id="chart_div2">
+    </div>
+  </div>
   </body>
 </html>
--- index.php
+++ index.php
@@ -4,8 +4,8 @@ ini_set('display_startup_errors',1);
 error_reporting(-1);
 require_once 'src/Google_Client.php';
 require_once 'src/contrib/Google_CalendarService.php';
+require_once 'src/contrib/Google_Oauth2Service.php';
 session_start();
-
 $client = new Google_Client();
 $client->setApplicationName("Google Calendar PHP Starter Application");
 
@@ -13,6 +13,9 @@ $client->setClientId('776685009882-v1kcqutjjt9k7h1e2qobjugqrgn9srel.apps.googleu
 $client->setClientSecret('sDK-e4GvFjp58ZTx23c4FQjz');
 //echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
 $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
+$client->setScopes(array('https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/plus.me',
+        'https://www.googleapis.com/auth/calendar'));  
 //echo "Waassaap";
 // Visit https://code.google.com/apis/console?api=calendar to generate your
 // client id, client secret, and to register your redirect uri.
@@ -21,12 +24,17 @@ $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']
 // $client->setRedirectUri('insert_your_oauth2_redirect_uri');
 // $client->setDeveloperKey('insert_your_developer_key');
 $cal = new Google_CalendarService($client);
+
 if (isset($_GET['logout'])) {
   unset($_SESSION['token']);
   unset($_SESSION['calendar']);
+  unset($_SESSION['email']);
 }
 if (isset($_GET['calendar'])) {
   $_SESSION['calendar']=$_GET['calendar'];
+}
+if (isset($_GET['change'])) {
+  unset($_SESSION['calendar']);
 }
 
 if (isset($_GET['code'])) {
@@ -40,20 +48,109 @@ if (isset($_SESSION['token'])) {
 }
 
 if ($client->getAccessToken()) {
-	$_SESSION['token'] = $client->getAccessToken();
-	if(isset($_SESSION['calendar'])){
-		echo("<script>window.location = 'home.php';</script>");
-	}
+  $_SESSION['token'] = $client->getAccessToken();
+  $oauth2 = new Google_Oauth2Service($client);
+  $user = $oauth2->userinfo->get();
+  $_SESSION['email'] = $user['email'];
+
+//print_r($user['email']);
+  if(isset($_SESSION['calendar'])){
+    echo("<script>window.location = 'home.php';</script>");
+  }
   $calList = $cal->calendarList->listCalendarList();
+  //print_r($_SESSION['token'], true);
+  //print(json_encode($client->getEmail()));
+
   $b = $calList['items'][0]['id'];
 
-  	echo '<form ><select name=calendar>';
+    $t = '<form ><table>
+  <tr>
+    <td><select name=calendar class="form-control" >';
    foreach ($calList['items'] as $a)
-  		echo '<option value='.$a['id'].'>'.$a['summary'].'</option>';
-	echo '</select><input type=submit></form>';
+      $t.= '<option value='.$a['id'].'>'.$a['summary'].'</option>';
+  $t.= '</select></td>
+    <td><input class="btn btn-default" type=submit></input></td>
+  </tr>
+</table></form>'; 
+  //echo '<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+ // <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>';
+
+   echo displ("Choose the calendar ".$t);
 //echo $a;
 $_SESSION['token'] = $client->getAccessToken();
 } else {
   $authUrl = $client->createAuthUrl();
-  print "<a class='login' href='$authUrl'>Connect Me!</a>";
+  //<INPUT type="submit" class="submit button" value="Google">
+  //<a href="$authUrl"> <button class='submit button'>Google</button></a>
+  $t = "<a href=$authUrl> <button class='btn btn-primary btn-lg'>Connect Google</button></a>";
+  //print "<a class='login' href='$authUrl'>Connect Me!</a>";
+  echo displ($t);
+}
+function displ($arg){
+  $email=$_SESSION['email'];
+  $str = <<<EOF
+
+  <style>
+  .content {
+       display: table-cell;
+    text-align: center;
+    vertical-align: middle;
+    }
+    html, body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    display: table
+}
+  </style>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+    </script>
+     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js">
+    </script>
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Call Log Analytics</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">How it works</a></li>
+      <li><a href="#">About Us</a></li>
+    </ul>
+    
+    <ul class="nav navbar-nav navbar-right" style="
+    padding-right: 20;
+">
+      
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">$email<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Calendar</a></li>
+          <li class="divider"></li>
+          <li><a href="./?logout">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
+    
+     
+
+    
+<div style=' width:600px; height:300px; position:absolute; left:46%; top:50%; margin-left:-100px; margin-top:-100px'>$arg</div>
+   
+  </body>
+
+EOF;
+return $str;
 }
